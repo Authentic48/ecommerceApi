6ea646bf2d9ad1cfd5f2d1649ad79d6587815921
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-07 13:10:06 +0700

    admin and seller middleware added

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -2,15 +2,16 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../Models/userModel.js')
 
+// @desc This middleware check login user 
 const protect = asyncHandler(async (req, res, next) => {
     let token
-    
+
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             req.user = await User.findById(decoded.id)
-            
+
             next()
         } catch (error) {
             console.error(error)
@@ -25,4 +26,24 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 })
 
-module.exports = { protect }
+// @des this middleware check admin
+const admin = asyncHandler(async (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error('Not authorized as an admin')
+    }
+})
+
+// @des this middleware check seller
+const seller = asyncHandler(async (req, res, next) => {
+    if (req.user && req.user.isSeller) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error('Not authorized as an seller')
+    }
+})
+
+module.exports = { protect, seller, admin }
--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const { getAllProducts, createProduct, updateProduct, getProductById, removeProduct } = require('../Controllers/productController')
-const { protect } = require('../Middlewares/authMiddleware')
+const { protect, seller, admin } = require('../Middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/').get(getAllProducts)
 
-router.route('/create').post(protect, createProduct)
+router.route('/create').post(protect, seller, createProduct)
 
 router.route('/:id')
-.get(getProductById)
-.put(protect, updateProduct)
-.delete(protect, removeProduct)
+    .get(getProductById)
+    .put(protect, seller, updateProduct)
+    .delete(protect, seller, removeProduct)
 
 module.exports = router;
