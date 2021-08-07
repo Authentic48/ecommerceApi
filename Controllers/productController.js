96f4f5888f2fa3249ba2431b5bd1b45b2a2a69e9
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-07 12:32:27 +0700

    Product added successfully

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,11 +1,31 @@
 const Product = require('../Models/productModel')
 const asyncHandler = require('express-async-handler')
 
+// @desc   Display all Products
+// @route   POST /api/products
+// @Access  Public
 const getAllProducts = asyncHandler(async (req, res) => {
 
     const products = await Product.find({})
     return res.status(201).json(products)
 })
 
-module.exports = { getAllProducts }
+const createProduct = asyncHandler(async (req, res) => {
+
+    const { name, image, description, brand, category, price, countInStock } = req.body;
+
+    const product = await Product.create({
+        name,
+        image,
+        description,
+        brand,
+        category,
+        price,
+        countInStock,
+        user: req.user._id
+    })
+
+    return res.status(201).json(product)
+})
+module.exports = { getAllProducts, createProduct }
 
--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { getAllProducts } = require('../Controllers/productController')
+const { getAllProducts, createProduct } = require('../Controllers/productController')
 const { protect } = require('../Middlewares/authMiddleware')
 
 const router = express.Router()
 
 router.route('/').get(getAllProducts)
 
-
+router.route('/create').post(protect, createProduct)
 module.exports = router;
