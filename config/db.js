03295689b5eb039ea87eedde1543b8a6f454413b
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-08 19:31:08 +0700

    user and password added to docker-compose

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ dotenv.config()
 const connectDB = async () => {
 
     try {
-        const connection = await mongoose.connect('mongodb://mongo:27017/ecommerceApi', {
+        const connection = await mongoose.connect('mongodb://admin:password@mongodb', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
@@ -22,5 +22,3 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB 
-
-// 'mongodb://mongo:27017/ecommerceApi'
