ee5bf695c7f7bb9a8d6f1e958057e0799dd6061c
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-08 20:26:32 +0700

    cloudbuild.yaml edited

--- utility/generateWebToken.js
+++ utility/generateWebToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 const generateWebToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET,  {
         expiresIn: '10d'
     })
 }
